5d2dba6f56f0d52d487b4136cd63ca3c0393c20a
programmer-priya <59862343+programmer-priya@users.noreply.github.com>
2021-05-03 20:44:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+var counter1 = 0;
+function image_1Click()
+{
+
+ 
+
+    console.log('Image clicked');
+    counter1 = counter1 +1;
+   
+    let span1 = document.getElementById("span1");
+   console.log(span1);
+    span1.innerHTML = counter1;
+    console.log(counter1);
+   
+}
+var counter2 = 0;
+function image_2Click()
+{
+
+ 
+
+    console.log('Image clicked');
+    counter2 = counter2 +1;
+   
+    let span2 = document.getElementById("span2");
+   console.log(span2);
+    span2.innerHTML = counter2;
+    console.log(counter2);
+   
+}
+var counter3 = 0;
+function image_3Click()
+{
+
+ 
+
+    console.log('Image clicked');
+    counter3 = counter3 +1;
+   
+    let span3 = document.getElementById("span3");
+   console.log(span3);
+    span2.innerHTML = counter3;
+    console.log(counter3);
+   
+}
+var counter4 = 0;
+function image_4Click()
+{
+
+ 
+
+    console.log('Image clicked');
+    counter4 = counter4 +1;
+   
+    let span4 = document.getElementById("span4");
+   console.log(span4);
+    span2.innerHTML = counter4;
+    console.log(counter4);
+   
+}
+
+
+
+
